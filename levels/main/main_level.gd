28ff12a0cd00edd2229b6fba7917f83415e91810
Miguel Silva <miguelklogsilva@gmail.com>
2025-02-23 10:17:27 +0000

    lightmaps

--- levels/main/main_level.gd
+++ levels/main/main_level.gd
@@ -1,1 +1,9 @@
 extends Node3D
+
+@export var world_env: WorldEnvironment
+
+func _input(event: InputEvent) -> void:
+	if event.is_action("debug_plus_expo"):
+		world_env.environment.tonemap_exposure += 0.05
+	elif event.is_action("debug_minus_expo"):
+		world_env.environment.tonemap_exposure -= 0.05
