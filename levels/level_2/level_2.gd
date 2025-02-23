cdaac26390c1ccf869e4ee5bdd913410b5bbadfb
Miguel Silva <miguelklogsilva@gmail.com>
2025-02-23 03:42:53 +0000

    cenas

--- levels/level_2/level_2.gd
+++ levels/level_2/level_2.gd
@@ -8,6 +8,16 @@ extends Node3D
 
 func _ready() -> void:
 	level_manager.objetive_just_completed.connect(on_completion)
+	var elevator: Elevator = get_tree()\
+	.get_first_node_in_group("Elevator")
+	elevator.area.body_exited.connect(
+		func(body: Node3D):
+			if not body is Player:
+				return
+			if not elevator.current_floor == Elevator.FLOOR_TYPE.FLOOR_2:
+				return
+			GlobalMusicPlayer.fade_to_track("Ethereal", 0.3, 5.0)
+	)
 
 var mat : StandardMaterial3D = preload("res://levels/materials/concrete_1/concrete_1.tres")
 func on_completion():
@@ -20,4 +30,5 @@ func on_completion():
 	for multi in multimeshes:
 		multi.gi_mode = GeometryInstance3D.GI_MODE_DISABLED
 	short_circuit_player.play()
+	GlobalMusicPlayer.stop()
 	
---
+++ levels/main/main_level.gd
@@ -0,0 +1,4 @@
+extends Node3D
+
+func _ready() -> void:
+	GlobalMusicPlayer.fade_to_track("Soft", 0.5, 7.0)
